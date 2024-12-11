78d302ae1592288637528233db49230a59a11200
sawsenFattahi <sawsen.fattahi@gmail.com>
2024-12-11 16:13:11 +0100

    init main server

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-//main server will be implimented here
+import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
 import server from "./server";
+
+dotenv.config();
+
+if(!process.env.PORT) {
+    throw new Error("PORT is not defined");
+    process.exit(1);
+}
+
+if(!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is not defined");
+    process.exit(1);
+}
+
+const PORT = parseInt(process.env.PORT) || 3000;
+const getMongoDataService = async () => {
+    const uri = process.env.MONGODB_URI || "";
+    const client = new MongoClient(uri);
+    await client.connect();
+    return client.db("chef-backend");
+};
+
+(async () => {
+    const mongodb = await getMongoDataService();
+    server.listen(PORT, () => {
+        console.log(`server is listening on port: ${PORT}`);
+    });
+})();
